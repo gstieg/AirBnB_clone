364a2336a3a3e59673cdaa8038c571dc8723a3ee
shaadibo <colbertrashaad@gmail.com>
2019-06-27 20:32:46 +0000

    base model w kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,22 @@ for other classes'''
 class BaseModel():
     ''''''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''intializing values'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            for key, value in kwargs.items():
+                if key == 'updated_at' or key == 'created_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key is not '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         '''returns string representation'''
-        return "[{}] ({}) {}".format/
+        return "[{}] ({}) {}".format\
         (self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
