361d7941ffe45d699b246cda818e17efb17806b3
Grayson Stiegler <682@holbertonschool.com>
2019-07-04 06:46:43 +0000

    fix pep8

--- models/base_model.py
+++ models/base_model.py
@@ -10,22 +10,16 @@ for other classes'''
 class BaseModel():
     ''''''
 
-
     def __init__(self, *args, **kwargs):
         '''intializing values'''
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-<<<<<<< HEAD
+
             models.storage.new(self)  # stores new instance
-=======
-<<<<<<< HEAD
-            models.storage.new(self) #stores new instance
-=======
             models.storage.new(self)  # stores new instance
->>>>>>> 9c6c762ac0b807d64026ceec308a75cd885238e5
->>>>>>> c438ae256c33bfdf7d9d305ebe433e8407b8e50d
+            models.storage.new(self)  # stores new instance
         else:
             for key, value in kwargs.items():
                 if key == 'updated_at' or key == 'created_at':
@@ -35,11 +29,10 @@ class BaseModel():
 
     def __str__(self):
         '''returns string representation'''
-<<<<<<< HEAD
+
         name_of_class = self.__class__.__name__
         return "[{}] ({}) {}".format(name_of_class, self.id, self.__dict__)
-=======
-<<<<<<< HEAD
+
         return "[{}] ({}) {}".format
 
     def __init__(self):
@@ -53,11 +46,9 @@ class BaseModel():
         return "[{}] ({}) {}".format
 
         (self.__class__.__name__, self.id, self.__dict__)
-=======
+
         name_of_class = self.__class__.__name__
         return "[{}] ({}) {}".format(name_of_class, self.id, self.__dict__)
->>>>>>> 9c6c762ac0b807d64026ceec308a75cd885238e5
->>>>>>> c438ae256c33bfdf7d9d305ebe433e8407b8e50d
 
     def save(self):
         '''saves the current time of creation'''
