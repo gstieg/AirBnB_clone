b25d385705e19b92c78676d93b459412188b87e6
shaadibo <colbertrashaad@gmail.com>
2019-07-05 17:38:52 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,12 @@ for other classes'''
 
 
 class BaseModel():
-    ''''''
-
     def __init__(self, *args, **kwargs):
         '''intializing values'''
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
-            models.storage.new(self)  # stores new instance
-            models.storage.new(self)  # stores new instance
             models.storage.new(self)  # stores new instance
         else:
             for key, value in kwargs.items():
@@ -29,26 +24,8 @@ class BaseModel():
 
     def __str__(self):
         '''returns string representation'''
-
-        name_of_class = self.__class__.__name__
-        return "[{}] ({}) {}".format(name_of_class, self.id, self.__dict__)
-
-        return "[{}] ({}) {}".format
-
-    def __init__(self):
-        '''intializing values'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
-    def __str__(self):
-        '''returns string representation'''
-        return "[{}] ({}) {}".format
-
-        (self.__class__.__name__, self.id, self.__dict__)
-
-        name_of_class = self.__class__.__name__
-        return "[{}] ({}) {}".format(name_of_class, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         '''saves the current time of creation'''
